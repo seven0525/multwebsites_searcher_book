4f0eec2eb3892cd47bc032c8047d61c01499a2c9
Taichi Watanabe <seven0511pman@gmail.com>
2018-10-28 10:58:44 +0900

    Flask用に修正
    Flask用に修正

--- bookoff_scriping.py
+++ bookoff_scriping.py
@@ -2,8 +2,8 @@ import requests
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-def amazon_get(key):
-    r = requests.get("http://www.bookoffonline.co.jp/display/L001,q={}".format(key))
+def main(key_word):
+    r = requests.get("http://www.bookoffonline.co.jp/display/L001,q={}".format(key_word))
     soup = BeautifulSoup(r.content, 'html.parser')
     titleTags = soup.find_all('p', class_="itemttl")
     names = []
@@ -14,7 +14,6 @@ def amazon_get(key):
     for d_name in names:
         if d_name not in names_uniq:
             names_uniq.append(d_name)
-    print(names_uniq)
+    # print(names_uniq)
 
-key_word = input("検索したい本のキーワードを入力してください")
-amazon_get(key_word)
+    return names_uniq[0], names_uniq[1], names_uniq[2], names_uniq[3], names_uniq[4]
