8dab68e762612fd67194d5837a3b2ba2f6291b96
Taichi Watanabe <seven0511pman@gmail.com>
2018-10-28 10:56:36 +0900

    Flaskように修正
    Flaskように修正

--- TAT_scriping.py
+++ TAT_scriping.py
@@ -2,8 +2,8 @@ import requests
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-def amazon_get(key):
-    r = requests.get("https://libwebservice.biblio.tuat.ac.jp/opac/opac_search/?lang=0&amode=2&appname=Netscape&version=5&cmode=0&smode=0&kywd={}".format(key))
+def main(key_word):
+    r = requests.get("https://libwebservice.biblio.tuat.ac.jp/opac/opac_search/?lang=0&amode=2&appname=Netscape&version=5&cmode=0&smode=0&kywd={}".format(key_word))
     soup = BeautifulSoup(r.content, 'html.parser')
     titleTags = soup.find_all('span', class_="tit_name")
     names = []
@@ -14,7 +14,8 @@ def amazon_get(key):
     for d_name in names:
         if d_name not in names_uniq:
             names_uniq.append(d_name)
-    print(names_uniq)
+    # print(names_uniq)
+    return names_uniq[0], names_uniq[1], names_uniq[2], names_uniq[3], names_uniq[4]
 
-key_word = input("検索したい本のキーワードを入力してください")
-amazon_get(key_word)
+# key_word = input("検索したい本のキーワードを入力してください")
+# amazon_get(key_word)
