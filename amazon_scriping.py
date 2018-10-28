207e15c3b1910f2218404bdae1d09080859c7098
Taichi Watanabe <seven0511pman@gmail.com>
2018-10-28 10:57:54 +0900

    Flask用に修正
    Flask用に修正

--- amazon_scriping.py
+++ amazon_scriping.py
@@ -14,14 +14,20 @@ amazon = AmazonAPI(AMAZON_ACCESS_KEY, AMAZON_SECRET_KEY, AMAZON_ASSOCIATE_TAG, R
 # search()メソッドでItemSearch操作を使い、商品情報を検索する。
 # キーワード引数Keywordsで検索語句を、SearchIndexで検索対象とする商品のカテゴリを指定する。
 # SearchIndex='All'はすべてのカテゴリから検索することを意味する。
-products = amazon.search(Keywords='python', SearchIndex='Books')
 
-count = 0
-for product in products:  # 得られた商品（AmazonProductオブジェクト）について反復する。
-    count += 1
-    if count > 3: # 検索結果のうち上位3つのみを表示
-        break
-    print(product.title)      # 商品名を表示。
-    print(product.offer_url)  # 商品のURLを表示。
-    price, currency = product.price_and_currency
-    print(price, currency)    # 価格と通貨を表示。
+titles = []
+
+def main(key_word):
+    products = amazon.search(Keywords=key_word, SearchIndex='Books')
+    count = 0
+    for product in products:  # 得られた商品（AmazonProductオブジェクト）について反復する。
+        count += 1
+        if count > 5: # 検索結果のうち上位3つのみを表示
+            break
+        titles.append(product.title)
+    
+    return titles[0], titles[1], titles[2], titles[3], titles[4]
+        # print(product.title)      # 商品名を表示。
+        # print(product.offer_url)  # 商品のURLを表示。
+        # price, currency = product.price_and_currency
+        # print(price, currency)    # 価格と通貨を表示。
